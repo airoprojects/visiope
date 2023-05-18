e8c08918f99f8cd977067fe6c599410ded903c38
leeoos <leonardo.colosi@gmail.com>
2023-05-18 17:23:35 +0000

    First test of loss function on dummy data

--- loss/loss_classes.py
+++
@@ -1,9 +0,0 @@
-print("I work")
-import torch.nn as nn
-
-class LossClass:
-    """ this class implements different loss function 
-    both standard and classic, for semantic segmentation task """
-
-    def standard_cross_entropy(outputs, labels):
-        nn.CrossEntropyLoss()
