568a0b63409a468c5a35de5b9245d8e4ed8daaa3
BrFranceN <132705068+BrFranceN@users.noreply.github.com>
2023-05-25 17:52:11 +0200

    minor fix

--- dataloader/load.py
+++ dataloader/load.py
@@ -53,11 +53,11 @@ class Ai4MarsData(Dataset):
     
     def setDevice(self,device,which):
         if which==0:
-            self.X = X.to(device)
+            self.X = self.X.to(device)
         else:
-            self.y = y.to(device)
+            self.y = self.y.to(device)
 
-    def conversion(self,what):
+    def convertion(self,what):
         if(what==0):
             self.y = self.y.type(torch.DoubleTensor)
         else:
