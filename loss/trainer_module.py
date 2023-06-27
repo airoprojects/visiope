3e2d769943582d32321c9f668ddd4ef675a59519
leeoos <leonardo.colosi@gmail.com>
2023-06-27 22:56:14 +0000

    testing: new trainer module

--- loss/trainer_module.py
+++ loss/trainer_module.py
@@ -1,4 +1,4 @@
-""" Trainer module for MER-Segmentation """
+""" Trainer module for MER-Segmentation 2.0 """
 
 import sys
 import torch
@@ -15,162 +15,105 @@ current_file = Path(__file__).parent
 if IN_COLAB: sys.path.insert(0, '/content/drive/MyDrive/Github/visiope/loss')
 else: sys.path.insert(0, current_file)
 from custom_loss import *
-import custom_loss
 
-# Training functions
-def trainer(hyperparameters, model, multiple_epochs=False, epoch_index=0, tb_writer=0):
+class Trainer():
 
-    if multiple_epochs: 
-        train_multiple_epoch(hyperparameters, EPOCHS=100)
-    else:
-        train_one_epoch(parameters, epoch_index, tb_writer)
+    def __init__(self, loss_fn, optimizer, training_set, test_set):
+
+        # Initialization of training parameters
+        self.loss_fn = loss_fn
+        self.optimizer = optimizer
+        self.training_set = training_set
+        self.test_set = test_set
+
+    def __show_inputs__():
+        print("Class inputs are: \n \
+              1. loss function \n \
+              2. optimizer \n \
+              3. training set \n"   
+        )
+
+    # This function implements training for just one epoch
+    def train_one_epoch(self, model):
+   
+        # To keep track of the last loss when the function is executed 
+        # through multiple epochs
+        running_loss = 0.
+        last_loss = 0.
+
+        # Here, we use enumerate(training_loader) so that we can track 
+        # the batch index and do some intra-epoch reporting.
+        for i, data in enumerate(self.training_set):
+
+            # Every data instance is an (input, label) pair
+            inputs, labels = data
+
+            # Zero your gradients for every batch!
+            self.optimizer.zero_grad()
+
+            # Make predictions for this batch
+            outputs = model(inputs)
+
+            # Compute the loss and its gradients
+            loss = self.loss_fn(outputs, labels)
+            loss.backward()
+
+            # Adjust learning weights
+            self.optimizer.step()
+            
+            # Gather data and report every last batch
+            running_loss += loss.item()
+            if i % 1000 == 999:
+                last_loss = running_loss / 1000 # loss per batch
+                print('  batch {} loss: {}'.format(i + 1, last_loss))
+                running_loss = 0.
+            
+        return last_loss
     
-# IMPORTANT: FIND OUT ABOUT TB_WRITER
-def train_one_epoch(parameters, epoch_index, tb_writer):
-
-    # Initialization of training parameters
-    model = parameters['model']
-    loss_fn = parameters['loss']
-    optimizer = parameters['optimizer']
-    training_set = parameters['training']
-    device = parameters['device']
-
-    # To keep track of the last loss when the function is executed 
-    # through multiple epochs
-    running_loss = 0.
-    last_loss = 0.
-
-    # Here, we use enumerate(training_loader) so that we can track 
-    # the batch index and do some intra-epoch reporting.
-    for i, data in enumerate(training_set):
-
-        # Every data instance is an (input, label) pair
-        inputs, labels = data
-
-        ''' 
-        This part should be done by the dataloader on all the dataset
-        inputs = inputs.permute(0,3,1,2).to(device)
-        labels = labels.to(device)
-        '''
-
-        # Zero your gradients for every batch!
-        optimizer.zero_grad()
-
-        # Make predictions for this batch
-        outputs = model(inputs)
-
-        '''
-        This part should be done by the dataloader on all the dataset
-        labels = labels [None, :, :, :]
-        labels = labels.type(torch.DoubleTensor)
-        '''
-
-        # Compute the loss and its gradients
-        loss = loss_fn(outputs, labels)
-        loss.backward()
-
-        # Adjust learning weights
-        optimizer.step()
+    # This function implements training for multiple epochs
+    def train_multiple_epoch(self, model, EPOCHS=100):
         
-        '''
-        IMPORTANT: UNCOMMENT THIS PART AFTER FINDING OUT ABOUT TB_WRITER
-        '''
-        # Gather data and report every last batch
-        running_loss += loss.item()
-        if i % 1000 == 999:
-            last_loss = running_loss / 1000 # loss per batch
-            print('  batch {} loss: {}'.format(i + 1, last_loss))
-            tb_x = epoch_index * len(training_set) + i + 1
-            '''tb_writer.add_scalar('Loss/train', last_loss, tb_x)'''
-            running_loss = 0.
-        
-    return last_loss
+        # Initialization of report parameters
+        timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+        epoch_number = 0 # just a counter
+        best_tloss = 1_000_000.
 
+        for epoch in range(EPOCHS):
+            print('EPOCH {}:'.format(epoch_number + 1))
 
-def train_multiple_epoch(parameters, EPOCHS=100):
+            # Make sure gradient tracking is on, and do a pass over the data
+            model.train(True)
+            avg_loss = self.train_one_epoch(model)
 
-    # Initialization of training parameters
-    model = parameters['model']
-    loss_fn = parameters['loss']
-    test_set = parameters['validation']
-    device = parameters['device']
+            # We don't need gradients on to do reporting
+            model.train(False)
 
-    # Initialization of report parameters
-    timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
-    writer = 0 #SummaryWriter('runs/fashion_trainer_{}'.format(timestamp))
-    epoch_number = 0 # just a counter
-    best_tloss = 1_000_000.
+            # Test loss
+            running_tloss = 0.0
+            for i, tdata in enumerate(self.test_set):
 
-    for epoch in range(EPOCHS):
-        print('EPOCH {}:'.format(epoch_number + 1))
+                # Every data instance is a (test_input, test_label) pair
+                test_inputs, test_labels = tdata
 
-        # Make sure gradient tracking is on, and do a pass over the data
-        model.train(True)
-        avg_loss = train_one_epoch(epoch_number, writer)
+                # Model prediction
+                toutputs = model(test_inputs)
 
-        # We don't need gradients on to do reporting
-        model.train(False)
+                # Run test loss
+                test_loss = self.loss_fn(toutputs, test_labels)
+                running_tloss += test_loss
 
-        # Test loss
-        running_tloss = 0.0
-        for i, tdata in enumerate(test_set):
+            avg_tloss = running_tloss / (i + 1)
+            print('LOSS train {} valid {}'.format(avg_loss, avg_tloss))
+            
+            # Track best performance, and save the model's state
+            if avg_tloss < best_tloss:
+                best_tloss = avg_tloss
+                model_path = 'model_{}_{}'.format(timestamp, epoch_number)
+                torch.save(model.state_dict(), model_path)
+            
+            epoch_number += 1
 
-            # Every data instance is a (test_input, test_label) pair
-            tinputs, tlabels = tdata
-
-            '''
-            This part should be done by the dataloader on all the dataset
-            tlabels = tlabels.type(torch.DoubleTensor)
-            tlabels = tlabels.to(device)
-            '''
-
-            # Model prediction
-            toutputs = model(tinputs)
-
-            # Run test loss
-            tloss = loss_fn(toutputs, tlabels)
-            running_tloss += tloss
-
-        avg_tloss = running_tloss / (i + 1)
-        print('LOSS train {} valid {}'.format(avg_loss, avg_tloss))
-
-        '''
-        # IMPORTANT: UNCOMMENT THIS PART AFTER FINDING OUT ABOUT TB_WRITER
-        # Log the running loss averaged per batch
-        # for both training and validation
-        writer.add_scalars('Training vs. Validation Loss',
-                        { 'Training' : avg_loss, 'Validation' : avg_vloss },
-                        epoch_number + 1)
-        writer.flush()
-        '''
-        
-        # Track best performance, and save the model's state
-        if avg_tloss < best_vloss:
-            best_vloss = avg_tloss
-            model_path = 'model_{}_{}'.format(timestamp, epoch_number)
-            torch.save(model.state_dict(), model_path)
-        
-        epoch_number += 1
 
 if __name__ == '__main__':
 
-    # This variable needs to be initialized with dummy values/data to test loss integration
-
-    model = None
-    optimizer = None
-    loss_fn = None
-    training_set = None 
-    validation_set = None
-    device = None
-
-    parameters = {
-        'model' : model,
-        'loss' : loss_fn,
-        'optimizer' : optimizer,
-        'training' : training_set,
-        'validation': validation_set,
-        'device' : device
-    }
-
-    print(IN_COLAB)
-
+    trainer = Trainer()
