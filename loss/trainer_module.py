eded958a1fcc41000867a3a871cd4e162f5c7244
leeoos <leoanrdo.colosi@gmail.com>
2023-06-28 01:27:01 +0200

    update: new tests for trainer

--- loss/trainer_module.py
+++ loss/trainer_module.py
@@ -5,17 +5,6 @@ import torch
 from pathlib import Path
 from datetime import datetime
 
-# Check if the script is runned in local or on colab
-IN_COLAB = 'google.colab' in sys.modules
-
-# Path object for the current file
-current_file = Path(__file__).parent
-
-# Setup path for custom imports in local and colab
-if IN_COLAB: sys.path.insert(0, '/content/drive/MyDrive/Github/visiope/loss')
-else: sys.path.insert(0, current_file)
-from custom_loss import *
-
 class Trainer():
 
     def __init__(self, loss_fn, optimizer, training_set, test_set):
