ced2a8eec278f668e77fecac270979d4c9366ff7
leeoos <leoanrdo.colosi@gmail.com>
2023-07-11 20:59:00 +0200

    gather some resources

--- trainer/loss/loss.py
+++ trainer/loss/loss.py
@@ -3,6 +3,7 @@ to train MER-Segmentation to perform semantic segmentation over martian terrain
 images. """
 
 import torch
+import numpy as np
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.autograd import Variable
@@ -59,6 +60,7 @@ class GPTFocalLoss(nn.Module):
         return loss
 
 class FocalLoss(nn.Module):
+
     def __init__(self, gamma=0, alpha=None, size_average=True):
         super(FocalLoss, self).__init__()
         self.gamma = gamma
@@ -91,3 +93,31 @@ class FocalLoss(nn.Module):
 
 
 
+class SoftDice(nn.Module):
+
+    def forward(y_true, y_pred, epsilon=1e-6): 
+        ''' 
+        Soft dice loss calculation for arbitrary batch size, number of classes, and number of spatial dimensions.
+        Assumes the `channels_last` format.
+    
+        # Arguments
+            y_true: b x X x Y( x Z...) x c One hot encoding of ground truth
+            y_pred: b x X x Y( x Z...) x c Network output, must sum to 1 over c channel (such as after softmax) 
+            epsilon: Used for numerical stability to avoid divide by zero errors
+        
+        # References
+            V-Net: Fully Convolutional Neural Networks for Volumetric Medical Image Segmentation 
+            https://arxiv.org/abs/1606.04797
+            More details on Dice loss formulation 
+            https://mediatum.ub.tum.de/doc/1395260/1395260.pdf (page 72)
+            
+            Adapted from https://github.com/Lasagne/Recipes/issues/99#issuecomment-347775022
+        '''
+        
+        # skip the batch and class axis for calculating Dice score
+        axes = tuple(range(1, len(y_pred.shape)-1)) 
+        numerator = 2. * np.sum(y_pred * y_true, axes)
+        denominator = np.sum(np.square(y_pred) + np.square(y_true), axes)
+        
+        return 1 - np.mean((numerator + epsilon) / (denominator + epsilon)) # average over classes and batch
+        # thanks @mfernezir for catching a bug in an earlier version of this implementation!
